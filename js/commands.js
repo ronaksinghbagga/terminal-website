16f6f3811f8c081e31ac17aae967a628dabecb4e
Ronak Singh <ronak1851996@gmail.com>
2024-05-06 01:45:45 +0530

    initial commit

---
+++ js/commands.js
@@ -0,0 +1,66 @@
+var linkedin = "https://www.linkedin.com/in/navdeep-singh-bagga-92483814b/";
+var github = "https://github.com/ronaksinghbagga/";
+var email = 'mailto:ronak1851996@gmail.com';
+var fkcodes = 'https://fkcodes.com/';
+
+whois = [
+  "<br>",
+  "Hey, I'm Navdeep!👋",
+  "I am a Software developer, a lazy one!",
+  "One who doesn't even want to create his own website 😁 so just copied from",
+  '<a href="' + fkcodes + '" target="_blank">fkcodes' + "</a>",
+  "<br>"
+];
+
+whoami = [
+  "<br>",
+  "The paradox of “Who am I?” is: we never know, but, we constantly find out.",
+  "<br>"
+];
+
+social = [
+  "<br>",
+  'linkedin       <a href="' + linkedin + '" target="_blank">linkedin.com/in/navdeep-singh-bagga' + "</a>",
+  'github         <a href="' + github + '" target="_blank">github.com/ronaksinghbagga' + "</a>",
+  "<br>"
+];
+
+// secret = [
+//   "<br>",
+//   '<span class="command">sudo</span>           Only use if you\'re admin',
+//   "<br>"
+// ];
+
+// projects = [
+//   "<br>",
+//   "Still curating... most projects are offline, on GitHub, or confidential.",
+//   "<br>"
+// ];
+
+help = [
+  "<br>",
+  '<span class="command">whois</span>          Who is Navdeep?',
+  '<span class="command">whoami</span>         Who are you?',
+  // '<span class="command">video</span>          View YouTube videos',
+  '<span class="command">social</span>         Display social networks',
+  // '<span class="command">secret</span>         Find the password',
+  '<span class="command">projects</span>       View coding projects',
+  // '<span class="command">history</span>        View command history',
+  '<span class="command">help</span>           You obviously already know what this does',
+  '<span class="command">email</span>          Do not email me',
+  '<span class="command">clear</span>          Clear terminal',
+  // '<span class="command">banner</span>         Display the header',
+  "<br>",
+];
+
+banner = [
+  
+//   _   _          _   _         
+//  | | | |   ___  | | | |   ___  
+//  | |_| |  / _ \ | | | |  / _ \ 
+//  |  _  | |  __/ | | | | | (_) |
+//  |_| |_|  \___| |_| |_|  \___/ 
+                               
+  '<span class="color2">Welcome to my interactive web terminal.</span>',
+  "<span class=\"color2\">For a list of available commands, type</span> <span class=\"command\">'help'</span><span class=\"color2\">.</span>",
+];
